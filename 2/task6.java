adef347d20b29a91ed429c89315acee91a2ffda4
GhostRider <kutcevolov@icloud.com>
2021-02-18 22:37:47 +0300

    add 6 task

---
+++ 2/task6.java
@@ -0,0 +1,40 @@
+public class task6 {
+    public static void main(String[] args) {
+        char a = '*';
+        int b;
+        for (b = 0; b < 1; b++) {
+            System.out.print(" " + a + " ");
+        }
+        System.out.println(" ");
+        for (b = 1; b < 3; b++) {
+            System.out.print(" " + a + " ");
+        }
+        System.out.println(" ");
+        for (b = 1; b < 4; b++) {
+            System.out.print(" " + a + " ");
+        }
+        System.out.println(" ");
+        for (b = 1; b < 5; b++) {
+            System.out.print(" " + a + " ");
+        } System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        for (b = 1; b < 5; b++) {
+            System.out.print(" " + a + " ");
+        }
+        System.out.println(" ");
+        for (b = 1; b < 4; b++) {
+            System.out.print(" " + a + " ");
+        }
+        System.out.println(" ");
+        for (b = 1; b < 3; b++) {
+            System.out.print(" " + a + " ");
+        }
+        System.out.println(" ");
+        for (b = 0; b < 1; b++) {
+            System.out.print(" " + a + " ");
+        }
+    }
+}
